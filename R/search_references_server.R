0bee97da7b35cbef1542fd500b02ec77655ee134
NicolasJBM <nicolas.jb.mangin@gmail.com>
2022-09-06 11:29:19 +0200

    Initialize package.

---
+++ R/search_references_server.R
@@ -0,0 +1,172 @@
+#' @name search_references_server
+#' @title Filter a list of references
+#' @author Nicolas Mangin
+#' @description Mudule allowing the user to find references using various filters.
+#' @param id Character. ID of the module to connect the user interface to the appropriate server side.
+#' @param references Reactive. Function containing a list of references.
+#' @return Filtered list of references
+#' @importFrom dplyr all_of
+#' @importFrom dplyr arrange
+#' @importFrom dplyr desc
+#' @importFrom dplyr mutate_all
+#' @importFrom dplyr select
+#' @importFrom DT renderDataTable
+#' @importFrom shiny moduleServer
+#' @importFrom shiny NS
+#' @importFrom shiny reactive
+#' @importFrom shiny updateSelectInput
+#' @importFrom shiny updateSliderInput
+#' @export
+
+
+
+search_references_server <- function(id, references){
+  ns <- shiny::NS(id)
+  shiny::moduleServer(id, function(input, output, session) {
+    
+    year <- NULL
+    
+    # Key
+    after_key_filter <- shiny::reactive({
+      shiny::req(base::length(references()) > 1)
+      bibliogR::filter_references(
+        dataset = references(),
+        variable = "key",
+        filter_value = input$inrefkey,
+        filter_type = "pattern"
+      )
+    })
+    
+    
+    # Authors
+    after_authors_filter <- shiny::reactive({
+      shiny::req(base::length(after_key_filter()) > 1)
+      selection <- bibliogR::filter_references(
+        dataset = after_key_filter(),
+        variable = "author",
+        filter_value = input$inrefauthors,
+        filter_type = "pattern"
+      )
+      if (base::nrow(selection) > 0){
+        minimum <- base::min(selection$year)
+        maximum <- base::max(selection$year)
+      } else {
+        minimum <- 0
+        maximum <- 2021
+      }
+      shiny::updateSliderInput(
+        session,
+        "slctrefperiod",
+        min = minimum,
+        max = maximum,
+        value = c(minimum, maximum)
+      )
+      selection
+    })
+    
+    # Period
+    after_period_filter <- shiny::reactive({
+      shiny::req(base::length(after_authors_filter()) > 1)
+      selection <- bibliogR::filter_references(
+        dataset = after_authors_filter(),
+        variable = "year",
+        filter_value = input$slctrefperiod,
+        filter_type = "range"
+      )
+      if (base::nrow(selection) <= 200){
+        shiny::updateSelectInput(
+          session,
+          "slctrefkey",
+          choices = c("", base::unique(selection$key))
+        )
+      }
+      selection
+    })
+    
+    
+    # Title
+    after_title_filter <- shiny::reactive({
+      shiny::req(base::length(after_period_filter()) > 1)
+      bibliogR::filter_references(
+        dataset = after_period_filter(),
+        variable = "title",
+        filter_value = input$inreftitle,
+        filter_type = "pattern"
+      )
+    })
+    
+    # Abstract
+    after_abstract_filter <- shiny::reactive({
+      shiny::req(base::length(after_title_filter()) > 1)
+      selection <- bibliogR::filter_references(
+        dataset = after_title_filter(),
+        variable = "abstract",
+        filter_value = input$inrefabstract,
+        filter_type = "pattern"
+      )
+      shiny::updateSelectInput(
+        session,
+        "slctreffield",
+        choices = c("", base::unique(selection$field))
+      )
+      selection
+    })
+    
+    # Field
+    after_field_filter <- shiny::reactive({
+      shiny::req(base::length(after_abstract_filter()) > 1)
+      selection <- bibliogR::filter_references(
+        dataset = after_abstract_filter(),
+        variable = "field",
+        filter_value = input$slctreffield,
+        filter_type = "selection"
+      )
+      shiny::updateSelectInput(
+        session,
+        "slctrefjournal",
+        choices = c("", base::unique(selection$journal))
+      )
+      selection
+    })
+    
+    # Journal
+    after_journal_filter <- shiny::reactive({
+      shiny::req(base::length(after_field_filter()) > 1)
+      bibliogR::filter_references(
+        dataset = after_field_filter(),
+        variable = "journal",
+        filter_value = input$slctrefjournal,
+        filter_type = "selection"
+      )
+    })
+    
+    # Selection
+    output$reflist <- DT::renderDataTable({
+      shiny::req(base::length(after_journal_filter()) > 1)
+      selected_columns <- c(
+        "key", "title", "author", "year", "journal", "abstract"
+      )
+      if (base::nrow(after_journal_filter()) <= 250) {
+        reflist <- after_journal_filter() |>
+          dplyr::arrange(dplyr::desc(year)) |>
+          dplyr::select(dplyr::all_of(selected_columns))
+      } else {
+        reflist <- base::data.frame(
+          key = "Too many references:", title = "Please refine your search.",
+          author = NA, year = NA, journal = NA, abstract = NA
+        )
+      }
+      selected_references <- bibliogR::with_childrow(
+        x = dplyr::mutate_all(reflist, base::as.character),
+        vars = c(
+          "author", "year", "journal", "abstract"
+        ),
+        opts = base::list(pageLength = 10)
+      )
+      selected_references
+    })
+    
+    return(after_journal_filter)
+  })
+}
+
---
+++ R/search_references_ui.R
@@ -0,0 +1,102 @@
+#' @name search_references_ui
+#' @title Filter a list of references
+#' @author Nicolas Mangin
+#' @description Mudule allowing the user to find references using various filters.
+#' @param id Character. ID of the module to connect the user interface to the appropriate server side.
+#' @return Filter list of references
+#' @importFrom DT dataTableOutput
+#' @importFrom shiny column
+#' @importFrom shiny fluidRow
+#' @importFrom shiny icon
+#' @importFrom shiny NS
+#' @importFrom shiny selectInput
+#' @importFrom shiny sliderInput
+#' @importFrom shinyWidgets searchInput
+#' @export
+
+
+search_references_ui <- function(id){
+  ns <- shiny::NS(id)
+  base::list(
+    shiny::fluidRow(
+      shiny::column(
+        4,
+        shinyWidgets::searchInput(
+          inputId = ns("inrefkey"),
+          label = "Key:",
+          width = "100%",
+          placeholder = "Pattern to search",
+          btnSearch = shiny::icon("magnifying-glass"),
+          btnReset = shiny::icon("eraser")
+        ),
+        shinyWidgets::searchInput(
+          inputId = ns("inrefauthors"),
+          label = "Authors:",
+          width = "100%",
+          placeholder = "Pattern to search",
+          btnSearch = shiny::icon("magnifying-glass"),
+          btnReset = shiny::icon("eraser")
+        ),
+        shiny::sliderInput(
+          ns("slctrefperiod"),
+          "Period:",
+          min = 0,
+          max = 2021,
+          value = c(0,2021),
+          sep = "",
+          width = "100%"
+        ),
+        shinyWidgets::searchInput(
+          inputId = ns("inreftitle"),
+          label = "Title:",
+          width = "100%",
+          placeholder = "Pattern to search",
+          btnSearch = shiny::icon("magnifying-glass"),
+          btnReset = shiny::icon("eraser")
+        ),
+        shinyWidgets::searchInput(
+          inputId = ns("inrefabstract"),
+          label = "Abstract:",
+          width = "100%",
+          placeholder = "Pattern to search",
+          btnSearch = shiny::icon("magnifying-glass"),
+          btnReset = shiny::icon("eraser")
+        ),
+        shiny::selectInput(
+          ns("slctreffield"),
+          "Field:",
+          choices = "",
+          selected = "",
+          width = "100%"
+        ),
+        shiny::selectInput(
+          ns("slctrefjournal"),
+          "Journal:",
+          choices = "",
+          selected = "",
+          width = "100%"
+        )
+      ),
+      shiny::column(
+        8,
+        DT::dataTableOutput(ns("reflist"))#,
+        #shiny::tags$head(
+        #  shiny::tags$style(
+        #    "#filtref-reflist table .odd {
+        #    background-color: #345;
+        #    color: #FFF;
+        #  }
+          #filtref-reflist table .even {
+        #    background-color: #222d32;
+        #    color: #FFF;
+        #  }
+        #  ",
+        #  media="screen",
+        #  type="text/css"
+        #  )
+        #)
+      )
+    )
+  )
+}
+

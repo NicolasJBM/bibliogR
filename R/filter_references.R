9b92e8f3346a3a453aa67eaacdc89a3d051eb465
NicolasJBM <nicolas.jb.mangin@gmail.com>
2023-06-17 14:52:31 +0200

    UI: adapt reference search to control panel. FIX: warning message in filters.

--- R/filter_references.R
+++ R/filter_references.R
@@ -40,27 +40,19 @@ filter_references <- function(
       
       if (filter_value[1]){
         
-        dplyr::filter(
-          dataset,
-          dataset[, variable] == filter_value[1]
-        )
+        dataset[dataset[, variable] == filter_value[1],]
         
       } else dataset
       
     } else if (filter_type == "selection") {
       
-      dplyr::filter(
-        dataset,
-        dataset[, variable] == filter_value
-      )
+      dataset[dataset[, variable] == filter_value,]
       
     } else if (filter_type == "multiple") {
       
       rgx <- base::paste(filter_value, collapse = "|")
-      dplyr::filter(
-        dataset,
-        stringr::str_detect(base::unlist(dataset[, variable]), rgx)
-      )
+      
+      dataset[stringr::str_detect(base::unlist(dataset[, variable]), rgx),]
       
     } else if (filter_type == "pattern") {
       
@@ -92,20 +84,14 @@ filter_references <- function(
       
     } else if (filter_type == "range") {
       
-      dplyr::filter(
-        dataset,
-        dataset[, variable] >= filter_value[1],
-        dataset[, variable] <= filter_value[2]
-      )
+      dataset <- dataset[dataset[, variable] >= filter_value[1],]
+      dataset[dataset[, variable] <= filter_value[2],]
       
     } else { # so if value
       
       if (!base::is.na(filter_value[1])){
         
-        dplyr::filter(
-          dataset,
-          dataset[, variable] == filter_value[1]
-        )
+        dataset[dataset[, variable] == filter_value[1],]
         
       } else dataset
       
--- R/search_references_ui.R
+++ R/search_references_ui.R
@@ -21,86 +21,105 @@ search_references_ui <- function(id){
   base::list(
     shiny::fluidRow(
       shiny::column(
-        4,
-        shinydashboardPlus::box(
-          width = 12, title = "Filters", solidHeader = TRUE,
-          status = "navy", collapsible = FALSE, collapsed = FALSE,
-          height = "700px",
-          shinyWidgets::searchInput(
-            inputId = ns("inrefkey"),
-            label = "Key:",
-            width = "100%",
-            placeholder = "Pattern to search",
-            btnSearch = shiny::icon("magnifying-glass"),
-            btnReset = shiny::icon("eraser")
-          ),
-          shinyWidgets::searchInput(
-            inputId = ns("inrefauthors"),
-            label = "Authors:",
-            width = "100%",
-            placeholder = "Pattern to search",
-            btnSearch = shiny::icon("magnifying-glass"),
-            btnReset = shiny::icon("eraser")
-          ),
-          shiny::sliderInput(
-            ns("slctrefperiod"),
-            "Period:",
-            min = 0,
-            max = 2021,
-            value = c(0,2021),
-            sep = "",
-            width = "100%"
-          ),
-          shinyWidgets::searchInput(
-            inputId = ns("inreftitle"),
-            label = "Title:",
-            width = "100%",
-            placeholder = "Pattern to search",
-            btnSearch = shiny::icon("magnifying-glass"),
-            btnReset = shiny::icon("eraser")
-          ),
-          shinyWidgets::searchInput(
-            inputId = ns("inrefabstract"),
-            label = "Abstract:",
-            width = "100%",
-            placeholder = "Pattern to search",
-            btnSearch = shiny::icon("magnifying-glass"),
-            btnReset = shiny::icon("eraser")
-          ),
-          shiny::selectInput(
-            ns("slctreffield"),
-            "Field:",
-            choices = "",
-            selected = "",
-            width = "100%"
-          ),
-          shiny::selectInput(
-            ns("slctrefjournal"),
-            "Journal:",
-            choices = "",
-            selected = "",
-            width = "100%"
-          )
+        6,
+        shinyWidgets::searchInput(
+          inputId = ns("inrefkey"),
+          label = "Key:",
+          width = "100%",
+          placeholder = "Pattern to search",
+          btnSearch = shiny::icon("magnifying-glass"),
+          btnReset = shiny::icon("eraser")
         )
       ),
       shiny::column(
-        8,
-        DT::dataTableOutput(ns("reflist"))#,
-        #shiny::tags$head(
-        #  shiny::tags$style(
-        #    "#filtref-reflist table .odd {
-        #    background-color: #345;
-        #    color: #FFF;
-        #  }
+        6,
+        shinyWidgets::searchInput(
+          inputId = ns("inrefauthors"),
+          label = "Authors:",
+          width = "100%",
+          placeholder = "Pattern to search",
+          btnSearch = shiny::icon("magnifying-glass"),
+          btnReset = shiny::icon("eraser")
+        ),
+      )
+    ),
+    shiny::column(
+      12,
+      shiny::sliderInput(
+        ns("slctrefperiod"),
+        "Period:",
+        min = 0,
+        max = 2021,
+        value = c(0,2021),
+        sep = "",
+        width = "100%"
+      )
+    ),
+    shiny::fluidRow(
+      shiny::column(
+        6,
+        shinyWidgets::searchInput(
+          inputId = ns("inreftitle"),
+          label = "Title:",
+          width = "100%",
+          placeholder = "Pattern to search",
+          btnSearch = shiny::icon("magnifying-glass"),
+          btnReset = shiny::icon("eraser")
+        )
+      ),
+      shiny::column(
+        6,
+        shinyWidgets::searchInput(
+          inputId = ns("inrefabstract"),
+          label = "Abstract:",
+          width = "100%",
+          placeholder = "Pattern to search",
+          btnSearch = shiny::icon("magnifying-glass"),
+          btnReset = shiny::icon("eraser")
+        )
+      )
+    ),
+    shiny::fluidRow(
+      shiny::column(
+        6,
+        shiny::selectInput(
+          ns("slctreffield"),
+          "Field:",
+          choices = "",
+          selected = "",
+          width = "100%"
+        )
+      ),
+      shiny::column(
+        6,
+        shiny::selectInput(
+          ns("slctrefjournal"),
+          "Journal:",
+          choices = "",
+          selected = "",
+          width = "100%"
+        )
+      )
+    ),
+    shiny::column(
+      12,
+      DT::dataTableOutput(ns("reflist")),
+      shiny::tags$head(
+        shiny::tags$style(
+          "
+          #filtref-reflist table {
+            color: #FFF;
+          }
+          #filtref-reflist table .odd {
+            background-color: #345;
+          }
           #filtref-reflist table .even {
-        #    background-color: #222d32;
-        #    color: #FFF;
-        #  }
-        #  ",
-        #  media="screen",
-        #  type="text/css"
-        #  )
-        #)
+            background-color: #222d32;
+          }
+          ",
+          media="screen",
+          type="text/css"
+        )
       )
     )
   )
